db45885def5ed7c9e2becbd702a133bd38642853
jamera mcneil <j.mcneil04@gmail.com>
2021-03-01 15:30:53 -0500

    set up RESTFUL routes configured mongoose

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 // DEPENDENCIES//
 
 const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 // CONFIGURATION //
 
 const app = express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
+mongoose.connect('mongodb://localhost:27017/basiccrud', { useNewUrlParser: true});
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
 
 // Allow use of Heroku's port or your own local port, dependong on the environment
 const port = 3000;
@@ -13,6 +23,48 @@ const port = 3000;
 
 app.get('/', (req,res) => {
     res.send('This is a test.')
+});
+
+// index //
+
+app.get('/shops', (req, res) => {
+    res.send('This is where the shops will be')
+});
+
+// new //
+
+app.get('/shops/new', (req, res) => {
+    res.send('Add a new shop here')
+});
+
+// create //
+
+app.post('/shops/', (req,res) => {
+    console.log('The information from the new route will be posted using this route')
+});
+
+// show //
+
+app.get('/shops/:id', (req,res) => {
+    res.send('View a specific shop page here')
+});
+
+// delete //
+
+app.delete('/shops/:id', (req, res) => {
+    console.log('This route deletes any app')
+});
+
+// edit //
+
+app.get('/shops/:id/:edit', (req,res) => {
+    res.send('This is where the edit page goes')
+});
+
+// update //
+
+app.put('/shops/:id', (req, res) => {
+    console.log('This applies any changes to existing shop listings')
 })
 
 
