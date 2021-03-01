a21972fd79823c842b1c5fec2d4bdd5a35476d8e
jamera mcneil <j.mcneil04@gmail.com>
2021-03-01 10:02:34 -0500

    created a route and tested app was sucessfully set up

--- server.js
+++ server.js
@@ -2,9 +2,16 @@
 
 const express = require('express');
 
+// CONFIGURATION //
 
 const app = express();
 const port = 3000;
+
+// ROUTES //
+
+app.get('/', (req,res) => {
+    res.send('This is a test.')
+})
 
 
 app.listen(port, () => {
